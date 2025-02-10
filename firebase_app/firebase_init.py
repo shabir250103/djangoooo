27bd59caf2a80a9305847d83dc4ae5d4added546
shabir250103 <srpvt21@gmail.com>
2025-02-10 17:11:40 +0530

    Update firebase_init.py

--- firebase_app/firebase_init.py
+++ firebase_app/firebase_init.py
@@ -1,9 +1,13 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 
-# 🔹 Load Firebase Credentials
-FIREBASE_CONFIG_PATH = "D:/a/myproject/firebase_app/service.json"
+# Get the current script directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# 🔹 Initialize Firebase App
+# Construct the relative path
+FIREBASE_CONFIG_PATH = os.path.join(BASE_DIR, "service.json")
+
+# Initialize Firebase
 cred = credentials.Certificate(FIREBASE_CONFIG_PATH)
 firebase_admin.initialize_app(cred)
